8df37772a344bc8f7cba220248b8f5cb3de050fa
capsaicinoids <114053967+capsaicinoids@users.noreply.github.com>
2023-04-12 08:23:57 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@
 -- You can think of a Lua "table" as a dictionary like data structure the
 -- normal format is "key = value". These also handle array like data structures
 -- where a value with no key simply has an implicit numeric key
+-- Let us abuse this
 local config = {
 
   -- Configure AstroNvim updates
